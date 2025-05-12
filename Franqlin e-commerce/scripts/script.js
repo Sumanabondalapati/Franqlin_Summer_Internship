d4ebc5d1082d5ce4a5b694280acdb2218760b89b
Sumana Bondalapati <Sumanabondalapati@gmail.com>
2025-05-13 01:57:42 +0530

    Add files via upload

---
+++ Franqlin e-commerce/scripts/script.js
@@ -0,0 +1,8 @@
+function navigateTo(page) {
+    if (page === 'login') {
+      window.location.href = '/login.html'; // Update to your actual login page
+    } else if (page === 'register') {
+      window.location.href = '/register.html'; // Update to your actual register page
+    }
+  }
+  
